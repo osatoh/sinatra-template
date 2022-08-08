741007dd36a5af03b7b21228840291dbac2695a4
osatoh <sugar961216@gmail.com>
2022-08-08 23:18:20 +0900

    Add instance variable

--- app.rb
+++ app.rb
@@ -3,13 +3,15 @@ Bundler.require
 
 enable :sessions
 
-client = PG::connect(
-  :host => "localhost",
-  :user => ENV.fetch("USER", ""), # "#"の箇所はターミナルで whoami を実行した結果に置き換えてください
-  :password => "",
-  :dbname => "#", # "#"の箇所は自分で作成したデータベースの名前に置き換えてください
-  )
+# client = PG::connect(
+#   :host => "localhost",
+#   :user => ENV.fetch("USER", ""), # "#"の箇所はターミナルで whoami を実行した結果に置き換えてください
+#   :password => "",
+#   :dbname => "#", # "#"の箇所は自分で作成したデータベースの名前に置き換えてください
+#   )
 
 get '/' do
+  @name = "ここに名前を入れてください"
+
   return erb :index
 end
