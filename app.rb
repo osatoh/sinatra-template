8dabac8622742b4c813dfdd16391fc7cf3785fe5
osatoh <sugar961216@gmail.com>
2022-08-08 18:59:41 +0900

    add client

--- app.rb
+++ app.rb
@@ -3,6 +3,13 @@ Bundler.require
 
 enable :sessions
 
+client = PG::connect(
+  :host => "localhost",
+  :user => ENV.fetch("USER", ""), # "#"の箇所はターミナルで whoami を実行した結果に置き換えてください
+  :password => "",
+  :dbname => "#", # "#"の箇所は自分で作成したデータベースの名前に置き換えてください
+  )
+
 get '/' do
   return erb :index
 end
